457a4ec1f667a4cd484d726c6a0135e21b33a4c8
Thomas SAUSSOL <thomas.saussol@orange.fr>
2020-03-09 13:24:26 +0100

    job 01 finit

---
+++ jour01/job01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <script type="text/javascript">
+
+    var mess= alert("Hello Javascript!")
+
+
+    </script>
+  </head>
+  <body>
+
+  </body>
+</html>
