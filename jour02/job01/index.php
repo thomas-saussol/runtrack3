910bcf90a628082f532a956142c1bb5cab4f8d4e
Thomas SAUSSOL <thomas.saussol@orange.fr>
2020-03-10 10:47:51 +0100

    job01 fini

---
+++ jour02/job01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <script type="text/javascript" src="script.js"></script>
+  </head>
+  <body>
+
+<article id="citation">
+  La vie a beaucoup plus d'imagination que nous!
+</article>
+
+<button class="button" type="button" name="button" onclick="citation()">Clique ici!</button>
+  </body>
+</html>
---
+++ jour02/job01/script.js
@@ -0,0 +1,6 @@
+function citation(){
+var text = document.getElementById('citation').textContent;
+document.getElementsByClassName('button').innerHTML = console.log(text);
+
+
+}
