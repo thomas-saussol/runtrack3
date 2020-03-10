8165600ae4c421bd28956e7827d73bd7028e1b26
Thomas SAUSSOL <thomas.saussol@orange.fr>
2020-03-10 11:15:24 +0100

    job02 fini

---
+++ jour02/job02/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <script type="text/javascript" src="script.js"></script>
+  </head>
+  <body>
+
+    <style>
+    .mystyle {
+    display:none;
+    }
+    </style>
+
+<button onclick="showhide()">Magie!</button>
+
+    <article id="myDIV">
+  L'important n'est pas la chute, mais l'atterrissage.
+</article>
+
+
+<p>Apparition / Disparition</p>
+  </body>
+</html>
---
+++ jour02/job02/script.js
@@ -0,0 +1,4 @@
+function showhide() {
+   var element = document.getElementById("myDIV");
+   element.classList.toggle("mystyle");
+}
