6eaffbb970056772cda010e44b3298d9d45f8221
Thomas SAUSSOL <thomas.saussol@orange.fr>
2020-03-12 16:28:19 +0100

    job02 a l'aide j'y suis presque!

--- jour04/job02/script.js
+++ jour04/job02/script.js
@@ -1,4 +1,4 @@
- JsonValueKey = '{"name":"La Plateforme_", "address":"8 rue  hozier", "city":"Marseille", "nb_staff":"11", "creation":"2019"}';
+ JsonValueKey = '{"name":"La Plateforme_", "address":"8 rue d\' hozier", "city":"Marseille", "nb_staff":"11", "creation":"2019"}';
 
  const creation1 = JSON.parse(creation);
 
