f4ebecb0b0099929cac53a35f5ba89c6da4b8a11
Thomas SAUSSOL <thomas.saussol@orange.fr>
2020-03-12 15:54:31 +0100

    Je ne comprends pas pourquoi cela ne marche pas aidez moi j'y suis presque!

---
+++ jour04/job02/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script language="javascript" src="script.js"></script>
+
+</head>
+<body>
+
+   <input id="button" onclick="JsonValueKey()" type="button" value="cliquez"/>
+</body>
+
+</html>
---
+++ jour04/job02/script.js
@@ -0,0 +1,5 @@
+ var creation = '{"name":"La Plateforme_", "address":"8 rue  hozier", "city":"Marseille", "nb_staff":"11", "creation":"2019"}';
+ const creation1 = JSON.parse(creation);
+  function JsonValueKey(creation1,city){
+    console.log(creation1,city);
+  }
